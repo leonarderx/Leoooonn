3668c1fb8700222e0f6178d214eb657b1948bb0c
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-11-06 19:01:00 +0300

    Add files via upload

---
+++ Source.cpp23456.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	/*const int n = 10;
+	int k = 0;
+	int arr[n];
+	srand(time(nullptr));
+	for (int i = 0; i < n; i++){
+		arr[i] = rand()%51-25;
+		cout << arr[i] << " ";
+	}
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > 0) {
+			k = k + 1;
+
+		}
+	}                                                                 
+	cout << k;*/
+	//cam1
+	/*const int n = 10;       
+	int sum = 0;
+	int arr[n];
+	srand(time(nullptr));
+		for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 31 - 10;
+		cout << arr[i] << " ";
+	}
+	for (int i = 0; i < n; i++) {
+	if (arr[i] %2== 0) {
+	sum= sum + arr[i];
+
+		}
+	}
+	cout << sum;*/
+	//cam2
+	/*const int n = 10;
+	int h = 0;
+	int arr[n];
+	srand(time(nullptr));
+	for (int i = 0; i < n; i++) {
+	arr[i] = rand() %101-50 ;
+	cout << arr[i] << " ";
+	}
+		for (int i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+		h = h + 1;
+
+		}
+	}
+		cout << k;*/
+		//cam3
+		/*const int n = 10;
+		int st = 1;
+		int arr[n];
+		srand(time(nullptr));
+		for (int i = 0; i < n; i++) {
+		arr[i] = rand() %41-20 ;
+		cout << arr[i] << " ";
+		}
+			for (int i = 0; i < n; i++) {
+			if (arr[i] % 5 != 0) {
+			st = st * arr[i];
+
+			}
+		}
+			cout << st
+			;*/
+		
+
+
+
+
+
+
+
+
+
+
+
+		
